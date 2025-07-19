5b3cd3d6ea6de2e2b1c3b1e01d34971f985f5acb
mrbublos <skrendel@gmail.com>
2025-07-19 17:12:24 +0300

    new script

--- start_training_beam.sh
+++ start_training_beam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # python /app/character_training/otniel_scripts/caption_with_florence-2.py /dataset --output_dir /train_dataset
-accelerate launch --num_processes 1 --mixed_precision bf16 --num_cpu_threads_per_process 2 $2run.py $1
+accelerate launch --num_processes 1 --mixed_precision bf16 --num_cpu_threads_per_process 2 $2 $1
